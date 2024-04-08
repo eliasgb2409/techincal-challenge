cd317d64ff579fcc6567c51b381abd5d29c9fc28
eliasgb <eliasgb@ulrik.uio.no>
2024-04-08 20:20:02 +0200

    Improved performance with async and other params

--- index.py
+++ index.py
@@ -1,65 +1,78 @@
-from opensearchpy import OpenSearch
+from opensearchpy import OpenSearch, AsyncOpenSearch
 import json
 from dotenv import load_dotenv
 import os
 from opensearch_functions import create_index, index_documents, count_indicies, search
-
-
+import asyncio
+import aiofiles
          
-def load_json(path):
+async def load_json(path):
     '''
     Function for loading a json file. 
 
     Input (str): path to json-file
     Return: loaded json-file
     ''' 
-    with open(path, 'r') as file:
-        return json.load(file)
+    async with aiofiles.open(path, 'r') as file:
+        data = await file.read()
+        return json.loads(data)
     
 
-def main():
+async def main():
     """
     1. Create the client for OpenSearch
-    2. Index documents 
-    3. Count indicies
+    2. Creating index
+    3. Reading json-files
+    4. Index documents 
+    5. Count indicies
     """
 
     load_dotenv()
     ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
     ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
 
+    # Creating client
     host = 'localhost'
     port = 9200
     auth = (ADMIN_USERNAME, ADMIN_PASSWORD) 
-    client = OpenSearch(
+    client = AsyncOpenSearch(
         hosts = [{'host': host, 'port': port}],
         http_auth = auth,
         use_ssl = True,
         verify_certs = False,
         ssl_assert_hostname = False,
         ssl_show_warn=False,
+        http_compress = True, # enables gzip compression for request bodies
     )
 
-    info = client.info()
+    try:
+        
+        info = await client.info()
+        
+        index_name = "ardoq-challenge"
+        
+        # Creating index
+        await create_index(index_name, client)
+        
+        # Reading json-files
+        comp_documents = await load_json("components.json")
+        ref_documents = await load_json("references.json")
+        await client.indices.refresh(index="ardoq-challenge")
+        
+        # Import data to index and run the indexing-functions concurrently
+        await asyncio.gather(
+            index_documents("ardoq-challenge", client, comp_documents, "components.json"),
+            index_documents("ardoq-challenge", client, ref_documents, "references.json")
+        )
+        print("Done indexing...")
+        await count_indicies(index_name, client) #Printing out the number of documents in index
     
-    index_name = "ardoq-challenge"
-    
-    create_index(index_name, client)
-    
-     # Import data to index
-    documents = load_json("components.json")
-    ref_documents = load_json("references.json")
-    client.indices.refresh(index="ardoq-challenge")
-    
-    print("Indexing...")
-    print("Indexing components.json...")
-    index_documents("ardoq-challenge", client, documents)
-    print(f'\nIndexing references.json...')
-    index_documents("ardoq-challenge", client, ref_documents)
-    print("Done indexing...")
-    
-    count_indicies(index_name, client)
+    finally:
+        await client.close()
 
     
 if __name__ == "__main__":
-    main()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
+    loop.close()
--- opensearch_functions.py
+++ opensearch_functions.py
@@ -1,67 +1,73 @@
 from opensearchpy import helpers
 import json
+import time
 
-
-def create_index(name, client):
+async def create_index(name, client):
     '''
     Function for creating index. 
 
-    Input (str): name of index
+    Input (str): name of index, client
     Return: None
     '''
+    
     # Explicit mapping of index (tried to do dynamic, but got some errors due to some fields named _version, _id, etc.)
     # The mapping will tell the index how to store the documents 
     index_body = {
         "settings": {
             "number_of_shards": 2,
             "number_of_replicas": 1,
+            "refresh_interval": "30s", # Setting a higher refresh_interval to improve indexing performance
         }
     }
-    
+        
     index_name = name
     
-    try: # Create index in OpenSearch cluster with name of index and the body that we mapped 
-        response = client.indices.create(
-            index_name, 
-            body=index_body
-        )
-        print('\nCreating index:')
-        print(response)
+    try: # Create index in OpenSearch cluster with name of index and the body that we mapped
+        if not await client.indices.exists(index=index_name):
+            
+            response = await client.indices.create(
+                index_name, 
+                body=index_body
+            )
+            print('\nCreating index:')
+            print(response)
         
     except Exception as e: #If index exist, do nothing
         print(e)
         
 
        
-def delete_index(index_name, client):
+async def delete_index(index_name, client):
     '''
     Function for deleting an index. 
 
-    Input (str): name of index to delete
+    Input (str): name of index to delete, client
     Return: response of deletion
     ''' 
-    response = client.indices.delete(
+    response = await client.indices.delete(
         index = index_name
     )
     print(response)
     return response
     
       
-def index_documents(index_name, client, documents): 
+async def index_documents(index_name, client, documents, file_name): 
     '''
     Function for importing documents with bulk operation to an index. 
 
-    Input (str): name of index, client, documents
+    Input (str): name of index, client, documents to index, name of file to index
     Return: response of import
     ''' 
     
+    start_time = time.time()
     actions = [] #actions to be made when doing bulk opertions
+    
     
     # Iterate over every document in the json-file
     for document in documents:
         doc_id = document.pop("_id", None)  # Remove _id from document and use it in the action
         
-        # We face a naming conflict iwth the _version key in the json-files
+        # We face a naming conflict with the _version key in the json-files
         # OpenSearch reserves the _version field for its internal use and we therefore have to change its name before processing
         if "_version" in document:
             document["app_version"] = document.pop("_version")
@@ -80,25 +86,29 @@ def index_documents(index_name, client, documents):
         )
     
     # Perform bulk operation to client
-    response = helpers.bulk(client, actions)
+    response = await helpers.async_bulk(client, actions)
+    duration = time.time() - start_time
+    print(f'\nIndexing...')
+    print(f'Indexing {file_name}...')
     print("Response: ", response)
+    print(f'Bulk indexing completed in {format(duration,".2f")} seconds.')
     return response
 
 
-def count_indicies(index_name, client):
+async def count_indicies(index_name, client):
     '''
     Function for counting the number in an index. 
 
-    Input (str): name of index
+    Input (str): name of index, client
     Return: None
     '''
-    client.indices.refresh(index=index_name)
-    print(client.cat.count(index=index_name, format="json"))
+    await client.indices.refresh(index=index_name)
+    print(await client.cat.count(index=index_name, format="json"))
     
 
 # Search for data with API call
-def search(index_name, field, searchword, client):
-    resp = client.search(
+async def search(index_name, field, searchword, client):
+    resp = await client.search(
         index=index_name,
         body={
             "query": {
